03327f01a40ca0da696b4a22c6a01ede275f8670
Michael <rgfb35@durham.ac.uk>
2022-08-07 15:57:00 +0100

    change ConfigurePlugboard to take in properties

--- enigma-simulator/src/components/ConfigurePlugboard.js
+++ enigma-simulator/src/components/ConfigurePlugboard.js
@@ -1,15 +1,71 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Collapse } from 'react-bootstrap';
 
-export default function ConfigurePlugboard() {
-    const [open, setOpen] = useState(false)
-
-    const handleReset = (event) => {
+export default class ConfigurePlugboard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+        this.setOpen = this.setOpen.bind(this);
+    
+    }
+    setOpen = () => {
+        this.setState({
+            open: !this.state.open
+        })
+    }
+    render() {
+        return (
+            <div>
+                <button 
+                className="btn" 
+                onClick={this.setOpen}
+                aria-controls="configure-plugboard"
+                aria-expanded={this.state.open}>Configure</button>
+                <Collapse in={this.state.open}>
+                    <div id="configure-plugboard">
+                        <form>
+                            Connect up to 10 pairs
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
+                                        <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
+                                        <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
+                                        <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
+                                        <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
+                                        <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
+                                        <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
+                                        <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
+                                        <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
+                                        <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
+                                    </tr>
+                                    <tr>
+                                        <td align="center" colSpan="10">
+                                            <button type="submit" className="btn"  onClick={this.props.handleConnect}>Connect</button>
+                                            <button className="btn">Reset</button>
+                                        </td>
+                                        
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </form>
+              
+                    </div>
+                </Collapse>
+            </div>
+        )
+    }
+    /*
+    handleReset = (event) => {
         event.preventDefault();
         document.querySelectorAll('.pair').forEach((elem) => {
             elem.value = null;
         })
     }
+    /*
+    /*
     const handleClick = (event) => {
         event.preventDefault();
         const pairs = document.querySelectorAll('.pair')
@@ -50,47 +106,8 @@ export default function ConfigurePlugboard() {
             plugBoard[letters[i]] = letters[i+1];
             plugBoard[letters[i+1]] = letters[i];
         }
-    }
-    return (
-        <div>
-            <button 
-            className="btn" 
-            onClick={() => setOpen(!open)}
-            aria-controls="configure-plugboard"
-            aria-expanded={open}>Configure</button>
-            <Collapse in={open}>
-                <div id="configure-plugboard">
-                    <form>
-                        Connect up to 10 pairs
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
-                                    <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
-                                    <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
-                                    <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
-                                    <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
-                                    <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
-                                    <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
-                                    <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
-                                    <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
-                                    <td><input type="text" size="2" maxLength="2" className="pair noEnterSubmit"></input></td>
-                                </tr>
-                                <tr>
-                                    <td align="center" colSpan="10">
-                                        <button type="submit" className="btn"  onClick={this.props.handleConnect}>Connect</button>
-                                        <button className="btn" onClick={handleReset}>Reset</button>
-                                    </td>
-                                    
-                                </tr>
-                            </tbody>
-                        </table>
-                    </form>
-          
-                </div>
-            </Collapse>
-        </div>
-    )
+    }*/
+    
 
 
 }
