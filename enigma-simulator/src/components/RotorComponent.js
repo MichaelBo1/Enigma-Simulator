4a10471864b3382dd592e0c2d0dba98950d3ae5f
Michael <rgfb35@durham.ac.uk>
2022-08-09 10:49:33 +0100

    add styling for settings next to rotor icons

--- enigma-simulator/src/components/RotorComponent.js
+++ enigma-simulator/src/components/RotorComponent.js
@@ -2,16 +2,30 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGear } from '@fortawesome/free-solid-svg-icons'
 
+
 class RotorComponent extends React.Component {
+    
     render() {
         return (
-            <div>
-                <p class="text-uppercase fw-bold">
-                    {this.props.position}
-                    -
-                    {this.props.ring}
-                </p>
-                <FontAwesomeIcon icon={faGear} className="fa-4x"/>
+            <div className="row">
+                <div className="col text-uppercase fw-bold pt-3 ps-4">
+                    <div className="vstack gap-0">
+                        <button className="text-btn">+</button>
+                        {this.props.position}
+                        <button className="text-btn">-</button>
+                    </div>
+                    
+                </div>
+                <div className="col fw-bold pt-3 px-1">
+                    <div className="vstack gap-0">
+                        <button className="text-btn">+</button>
+                        {this.props.ring}
+                        <button className="text-btn">-</button>
+                    </div>
+                </div>
+                <div className="col pt-3">
+                    <FontAwesomeIcon icon={faGear} className="fa-4x"/>
+                </div>
             </div>
             
            
