9517e485550188ab9e90b8b8989ab59e7075cb54
Michael <rgfb35@durham.ac.uk>
2022-08-14 17:37:52 +0100

    define rotor instances separately

---
+++ enigma-simulator/src/DefinedRotors.js
@@ -0,0 +1,24 @@
+import Rotor from "./logic/Rotor.js";
+
+const rotorI = new Rotor(['e', 'k', 'm', 'f', 'l', 'g', 'd', 'q', 'v', 'z', 'n', 't', 'o', 'w', 'y', 'h', 'x', 'u', 's', 'p', 'a', 'i', 'b', 'r', 'c', 'j'], 'a', 1, 'q');
+const rotorII = new Rotor(['a', 'j', 'd', 'k', 's', 'i', 'r', 'u', 'x', 'b', 'l', 'h', 'w', 't', 'm', 'c', 'q', 'g', 'z', 'n', 'p', 'y', 'f', 'v', 'o', 'e'], 'a', 1, 'e');
+const rotorIII = new Rotor(['b', 'd', 'f', 'h', 'j', 'l', 'c', 'p', 'r', 't', 'x', 'v', 'z', 'n', 'y', 'e', 'i', 'w', 'g', 'a', 'k', 'm', 'u', 's', 'q', 'o'], 'a', 1, 'v');
+const rotorIV = new Rotor(['e', 's', 'o', 'v', 'p', 'z', 'j', 'a', 'y', 'q', 'u', 'i', 'r', 'h', 'x', 'l', 'n', 'f', 't', 'g', 'k', 'd', 'c', 'm', 'w', 'b'], 'a', 1, 'j');
+const rotorV =  new Rotor(['v', 'z', 'b', 'r', 'g', 'i', 't', 'y', 'u', 'p', 's', 'd', 'n', 'h', 'l', 'x', 'a', 'w', 'm', 'j', 'q', 'o', 'f', 'e', 'c', 'k'], 'a', 1, 'z');
+
+export default function getRotor(num) {
+    switch (num) {
+        case 1:
+            return(rotorI);
+        case 2:
+            return(rotorII)
+        case 3:
+            return(rotorIII);
+        case 4:
+            return(rotorIV);
+        case 5:
+            return(rotorV);
+        default:
+            return null;
+    }
+}
